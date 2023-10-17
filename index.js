0fa60bbb5f29b221a6422bf5c7e6f6682062719f
josephmangara <josephmangara2@gmail.com>
2023-10-17 08:52:58 +0300

    wip

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 //fetch requests data from the json live server.
 //.then assists in getting a response from json.
-const baseURL = 'http://localhost:3000/characters'
+let characters = []
+document.addEventListener("DOMContentLoaded", function(){
+    getCharacters()
+})
 
-fetch(baseURL)
+function getCharacters(){
+fetch("http://localhost:3000/characters")
 .then(res => res.json())
 .then(data => {
  displayContent(data)
-})
+})}
 
 function displayContent(data) {
     for (let i of data){
@@ -16,12 +20,26 @@ function displayContent(data) {
     let li = document.createElement('li');
     li.textContent = i.name;
     list.appendChild(li);
-const voteCounter = document.getElementsByTagName('li')
-let voteCount = 0;
-li.addEventListener('click', ()=> {
-    voteCount = voteCount++;
-})
+}}
+// function displayCharacterDetails(characters){
+//     document.getElementsId('data')
+//     console.log(data)
+//     document.addEventListener('click', )
+//      alert('it\'s working')
+// }
 
-    }
-   }
-  
+const button = document.getElementsById('button').addEventListener
+    ('submit',(event) => alert("vote counted"));
+    console.log(button)
+// function addingEventListener() {
+//     const button = document.getElementById('button');
+//     button.addEventListener('click', function() {
+//       alert('I was clicked!');
+//     });
+//     }
+
+
+const voteCounter = document.querySelector('data')
+li.addEventListener('click', ()=> {
+    document.querySelector('#image')
+    image.src = object.image})
